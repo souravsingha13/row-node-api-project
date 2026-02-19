b8e8ee4ef6c88166c22a467fc13c383ae339b413
souravsingha13 <souravsingha1304021@gmail.com>
2026-02-19 16:07:17 +0600

    :construction: Ready the process request

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const https = require('https');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
@@ -34,9 +35,23 @@ app.handleReqRes = (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathName = parsedUrl.pathname;
     const trimmedPath = pathName.replace(/^\/+|\/+$/g, '');
-    console.log(trimmedPath);
-    // response handling
-    res.end('Hello World!');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    })
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handling
+        res.end('Hello World!');
+    });
 }
 
 app.createServer();
